1a73d24f5ca593a8ee402d13d8c3fa4400e282af
Alexey Melnichuk <mimir@newmail.ru>
2015-10-01 15:05:27 +0400

    Add. dummy test

--- test/test_odbc.lua
+++ test/test_odbc.lua
@@ -1,2 +1,17 @@
-local uv = require "lluv"
-uv.odbc  = require "lluv.odbc"
+local uv   = require "lluv"
+local odbc = require "lluv.odbc"
+
+local cnn = odbc.connection.new()
+
+local CNN = {
+  {Driver = IS_WINDOWS and '{MySQL ODBC 5.2 ANSI Driver}' or 'MySQL'};
+  {db='test'};
+  {uid='root'};
+};
+
+cnn:connect(CNN, function(...)
+  print(...)
+  cnn:close()
+end)
+
+uv.run()
